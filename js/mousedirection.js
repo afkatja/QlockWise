1a9b471aa8ef5bc3244eb89e915948e08b385792
Katja Hollaar <katja@q42.nl>
2013-11-09 12:11:56 +0100

    backgrounds, parallax effect, video positioning

---
+++ js/mousedirection.js
@@ -0,0 +1,36 @@
+(function ($) {
+    var options = {};
+    var oldx = 0;
+    var direction = "";
+    var stop_timeout = false;
+    var stop_check_time = 150;
+    $.mousedirection = function (opts) {
+        var defaults = {};
+        options = $.extend(defaults, opts);
+        $(document).on("mousemove", function (e) {
+            var activeElement = e.target || e.srcElement;
+            if (e.pageX > oldx) {
+                direction = "right";
+            } else if (e.pageX < oldx) {
+                direction = "left";
+            }
+
+            clearTimeout(stop_timeout);
+            stop_timeout = setTimeout(function () {
+                direction = "stop";
+                $(activeElement).trigger(direction);
+                $(activeElement).trigger({
+                    type: "mousedirection",
+                    direction: direction
+                });
+            }, stop_check_time);
+
+            $(activeElement).trigger(direction);
+            $(activeElement).trigger({
+                type: "mousedirection",
+                direction: direction
+            });
+            oldx = e.pageX;
+        });
+    }
+})(jQuery)
